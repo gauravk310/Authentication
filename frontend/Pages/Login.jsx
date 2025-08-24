65ee42810bc623f7567917aaefe2d7af4e244ae8
Gaurav Kadam <gskadam3b@gmail.com>
2025-08-24 13:33:12 +0530

    new

--- frontend/Pages/Login.jsx
+++ frontend/Pages/Login.jsx
@@ -28,7 +28,7 @@ export const Login = () => {
     }
     try {
       setLoading(true);
-      const url = "https://authentication-api-seven.vercel.app/auth/login";
+      const url = "https://authentication-api-one.vercel.app/auth/login";
       const response = await fetch(url, {
         method: "POST",
         headers: {
